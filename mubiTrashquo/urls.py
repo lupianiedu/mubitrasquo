6bc633d6deac6e01745647f36f8cb34f100ba588
Eduardo Lupiani <eduardo.lupiani@mubiquo.com>
2015-02-27 18:14:54 +0100

    Improve support for logged users

--- mubiTrashquo/urls.py
+++ mubiTrashquo/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+
+# Create a new class that redirects the user to the index page, if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self,   request, user):
+        return '/trash/'
 
 urlpatterns = patterns('',
                        # Examples:
@@ -8,5 +15,6 @@ urlpatterns = patterns('',
 
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^trash/', include('trash.urls')),
+                       url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
                        url(r'^accounts/', include('registration.backends.simple.urls')),
 )
--- trash/views.py
+++ trash/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'trash/index.html')
+    model = {}
+    if not request.user.is_authenticated():
+        return redirect('/accounts/login/')
+    else:
+        model['username'] = request.user.username
+
+    return render(request, 'trash/index.html', model)
